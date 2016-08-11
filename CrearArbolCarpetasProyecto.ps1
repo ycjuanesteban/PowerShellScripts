783120d8ecad820380fda2cf531a907e15bcc296
JuanEstebanYC <ycjuanesteban@gmail.com>
2016-08-11 08:38:33 -0500

    Add files via upload
    Script que permite crear un árbol básico de carpetas para contener un proyecto.
    Pendiente de posibles modificaciones para añadir más carpetas y sus subcarpetas requeridas.

---
+++ CrearArbolCarpetasProyecto.ps1
@@ -0,0 +1,23 @@
+﻿<# 
+.SYNOPSIS
+  Script que ayuda a crear un arbol de carpetas necesarias para un proyecto de desarrollo
+.NOTES 
+    File Name      : CrearArbolCarpetasProyecto.ps1  
+    Author         : Juan Esteban Yarce Carmona (https://github.com/JuanEstebanYC)
+#>
+
+$RUTAPARCIAL = "D:\\";
+$NOMBREPROYECTO = "PRUEBA DESARROLLO";
+$RUTACOMPLETA = "$RUTAPARCIAL$NOMBREPROYECTO";
+$CARPETAS = @("CODIGO","DOCUMENTACION","ENTREGAS","PRUEBAS");
+
+if(!(Test-Path -Path $RUTACOMPLETA)){
+    New-Item -ItemType directory -Path $RUTA
+    cd $RUTA
+    foreach($CARPETA in $CARPETAS){
+       New-Item -ItemType directory -Name $CARPETA; 
+    }
+
+}else{
+    Write-Host "La carpeta ya existe y no se puede crear" -ForegroundColor Red
+}
