eba7d7d354a79b26c4c6d161ccc99d310985ceb9
JuanEstebanYC <ycjuanesteban@gmail.com>
2016-11-15 22:00:33 -0500

    Se corrigen los errores encontrados
    [comentario]Se corrigen los errores encontrados

--- CrearArbolCarpetasProyecto.ps1
+++ CrearArbolCarpetasProyecto.ps1
@@ -1,4 +1,4 @@
-﻿<# 
+<# 
 .SYNOPSIS
   Script que ayuda a crear un arbol de carpetas necesarias para un proyecto de desarrollo
 .NOTES 
@@ -6,14 +6,14 @@
     Author         : Juan Esteban Yarce Carmona (https://github.com/JuanEstebanYC)
 #>
 
-$RUTAPARCIAL = "D:\\";
-$NOMBREPROYECTO = "PRUEBA DESARROLLO";
+$RUTAPARCIAL = "RUTA PROYECTO";
+$NOMBREPROYECTO = "NOMBRE PROYECTO";
 $RUTACOMPLETA = "$RUTAPARCIAL$NOMBREPROYECTO";
 $CARPETAS = @("CODIGO","DOCUMENTACION","ENTREGAS","PRUEBAS");
 
 if(!(Test-Path -Path $RUTACOMPLETA)){
-    New-Item -ItemType directory -Path $RUTA
-    cd $RUTA
+    New-Item -ItemType directory -Path $RUTACOMPLETA
+    cd $RUTACOMPLETA
     foreach($CARPETA in $CARPETAS){
        New-Item -ItemType directory -Name $CARPETA; 
     }
