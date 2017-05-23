0e2cff8d3150fdbec897d7a57733e51081bc358d
Fabricac <adminfcaf2@intergrupo.com>
2017-05-23 14:36:06 -0500

    Actualización de parametros

--- SHAREPOINT/WarmUp/Instalador.ps1
+++ SHAREPOINT/WarmUp/Instalador.ps1
@@ -1,10 +1,10 @@
 #ruta archivo Warmup
-$ruta = 'C:\SPUtilities\WarmUp\WarmUp.ps1';
+$ruta = '';
 
 $TareaTrigger = New-ScheduledTaskTrigger -Daily -At '6AM';
 $TareaNombre = "WarmUp Sharepoint";
 $Tarea = New-ScheduledTaskAction -Execute "powershell.exe" -File $ruta;
 $TareaConfiguracion = New-ScheduledTaskSettingsSet -DontStopOnIdleEnd -ExecutionTimeLimit ([TimeSpan]::Zero)
-$TareaPrincipal = New-ScheduledTaskPrincipal -User "INTERGRUPO\adminfcaf2" -RunLevel "Highest"
+$TareaPrincipal = New-ScheduledTaskPrincipal -User "Dominio\Usuario" -RunLevel "Highest"
 
 Register-ScheduledTask $TareaNombre -Action $Tarea -Trigger $TareaTrigger -Principal $TareaPrincipal -Settings $TareaConfiguracion
